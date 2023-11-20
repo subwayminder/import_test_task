351998e94a47a64671812d668bcfa31fdb36f223
Matvey Varenitsa <subwayminder@gmail.com>
2023-11-20 12:29:46 +0700

    Create import api route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DataExportController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,13 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-Route::get('test', function () {
-    return response()->json(
-        [
-            'success' => true
-        ]
-    );
-});
+Route::post('import', [DataExportController::class, 'index']);
